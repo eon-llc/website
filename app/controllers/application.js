b4074cf8755f7a9ecaf876ff0ac41d313a69ca38
Serg Chernata <sergchernata@gmail.com>
2019-07-09 17:56:07 -0400

    detect indx route and toggle mobile nav

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,11 @@ import { equal } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  isIndex: equal('currentRouteName', 'index')
-
+    isIndex: equal('currentRouteName', 'index'),
+    actions: {
+        toggleMobileNav() {
+            document.getElementById("mobile-nav").classList.toggle("show");
+            document.getElementById("mobile-toggle").classList.toggle("open");
+        }
+    }
 });
