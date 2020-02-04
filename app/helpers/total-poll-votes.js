19b8053a12b79dc50f8bb11e93efdf70e0bf89df
Serg Chernata <sergchernata@gmail.com>
2020-02-04 07:38:27 -0500

    added total poll votes helper

---
+++ app/helpers/total-poll-votes.js
@@ -0,0 +1,18 @@
+import { helper } from '@ember/component/helper';
+
+export function totalPollVotes(params) {
+  const [ options, is_token_poll ] = params;
+  let total = 0;
+
+  for(let i=0; i<options.length; i++) {
+    if(is_token_poll) {
+      total += options[i].votes / 10000;
+    } else {
+      total += options[i].votes;
+    }
+  }
+
+  return total;
+}
+
+export default helper(totalPollVotes);
